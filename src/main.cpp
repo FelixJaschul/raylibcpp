a32693d4f48b89568f10227bbda4f54a61f05869
felixjaschul <felix@jaschul.de>
2026-01-15 19:11:31 +0100

    imgui + texture

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,48 @@
-#include <iostream>
-#include "raylib.h"
-#include "imgui.h"
-#include "rlImGui.h"
 #include "main.h"
 
 int main()
 {
+    // Init
     InitWindow(WIDTH, HEIGHT, "raycast");
     SetTargetFPS(60);
 
     rlImGuiSetup(true);
 
+    state.level.walls = new wall_t*[MAX_WALLS];
+    state.level.walls[0] = new wall_t;
+    state.level.walls[0]->texture = LoadTexture("res/stone.png");
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(DARKPURPLE);
+
+        DrawTexture(state.level.walls[0]->texture, 250, 50, WHITE);
+
+        // Update
+        state.m_pos = GetMousePosition();
 
         rlImGuiBegin();
-        ImGui::ShowDemoWindow();
+        constexpr float padding = 10.0f;
+        ImGui::SetNextWindowPos(ImVec2(padding, padding), ImGuiCond_Always);
+        ImGui::SetNextWindowSize(ImVec2(200, (float)GetScreenHeight() - 2 * padding), ImGuiCond_Always);
+        
+        if (ImGui::Begin("raycast", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse))
+        {
+            ImGui::Text("FPS: %d", GetFPS());
+            ImGui::Text("Mouse: %.0f, %.0f", state.m_pos.x, state.m_pos.y);
+            ImGui::Text("Looking at: Nothing");
+            ImGui::End();
+        }
+
         rlImGuiEnd();
         EndDrawing();
     }
+
+    // Cleanup
+    UnloadTexture(state.level.walls[0]->texture);
+    free(state.level.walls);
+    free(state.level.walls[0]);
 
     rlImGuiShutdown();
     CloseWindow();
